a2aa3b46ec381d1e205b36d02de6233ca70283ea
qubaitian <2501055750@qq.com>
2023-05-29 16:14:41 +0800

    使用这个库

--- lua/lazyvim/plugins/core.lua
+++ lua/lazyvim/plugins/core.lua
@@ -2,5 +2,5 @@ require("lazyvim.config").init()
 
 return {
   { "folke/lazy.nvim", version = "*" },
-  { "LazyVim/LazyVim", priority = 10000, lazy = false, config = true, cond = true, version = "*" },
+  { "git@github.com:qubaitian/LazyVim", priority = 10000, lazy = false, config = true, cond = true, version = "*" },
 }
